7e0c1ed6a648c70cdb9224822a3f7bb835864364
kaun <kaun@involve.software>
2023-10-20 17:24:28 +0100

    Added json config setup logic

--- app/src/main/java/com/vcheck/sdk/core/domain/VCheckDesignConfig.kt
+++ app/src/main/java/com/vcheck/sdk/core/domain/VCheckDesignConfig.kt
@@ -1,7 +1,9 @@
 package com.vcheck.sdk.core.domain
 
+import android.util.Log
 import com.google.gson.annotations.SerializedName
 import com.google.gson.Gson
+import com.vcheck.sdk.core.VCheckSDK
 
 data class VCheckDesignConfig (
     @SerializedName("primary")
@@ -87,7 +89,60 @@ data class VCheckDesignConfig (
     companion object {
         @JvmStatic
         fun fromJsonStr(rawJsonStr: String) : VCheckDesignConfig {
-            return Gson().fromJson(rawJsonStr, VCheckDesignConfig::class.java)
+            return try {
+                Gson().fromJson(rawJsonStr, VCheckDesignConfig::class.java)
+            } catch (e: Exception) {
+                Log.w(VCheckSDK.TAG, "VCheckSDK - warning: Non-valid JSON was passed while " +
+                        "initializing VCheckDesignConfig instance; trying to set VCheck default theme...")
+                Gson().fromJson(defaultThemeJsonStr, VCheckDesignConfig::class.java)
+            }
         }
+
+        private const val defaultThemeJsonStr: String =
+            """
+            {
+               "primary": "#2E75FF",
+               "primaryHover": "#2E96FF",
+               "primaryActive": "#3361EC",
+               "primaryContent": "#FFFFFF",
+               "primaryBg": "#5D6884",
+               "accent": "#6096FF",
+               "accentHover": "#6ABFFF",
+               "accentActive": "#4F79F7",
+               "accentContent": "#FFFFFF",
+               "accentBg": "#32404A",
+               "neutral": "#FFFFFF",
+               "neutralHover": "rgba(255, 255, 255, 0.4)",
+               "neutralActive": "rgba(255, 255, 255, 0.1)",
+               "neutralContent": "#000000",
+        
+               "success": "#6CFB93",
+               "successHover": "#C8FDD2",
+               "successActive": "#00DF53",
+               "successBg": "#3A4B3F",
+               "successContent": "#3B3B3B",
+               "error": "#F47368",
+               "errorHover": "#FF877C",
+               "errorActive": "#DE473A",
+               "errorBg": "#4B2A24",
+               "errorContent": "#3B3B3B",
+               "warning": "#FFB482",
+               "warningHover": "#FFBF94",
+               "warningActive": "#D3834E",
+               "warningBg": "#3F3229",
+               "warningContent": "#3B3B3B",
+        
+               "base": "#2A2A2A",
+               "base_100": "#3C3C3C",
+               "base_200": "#555555",
+               "base_300": "#6A6A6A",
+               "base_400": "#7F7F7F",
+               "base_500": "#949494",
+               "baseContent": "#FFFFFF",
+               "baseSecondaryContent": "#D8D8D8",
+               "disabled": "#AAAAAA",
+               "disabledContent": "#6A6A6A"
+            }
+            """
     }
 }
