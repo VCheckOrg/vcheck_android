ace857044ca16390f741ff7078ece6edc2b4b1a3
kaun <kaun@involve.software>
2023-12-04 13:46:35 +0000

    added test project base

---
+++ testapp/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+//TODO: move all code from demo project here and refactor keys / data
+//TODO: rename library directory (?)
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
